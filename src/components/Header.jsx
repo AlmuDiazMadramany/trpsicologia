de40dcc556c606d3b1db3c81b5e309a8e2262754
AlmuDiazMadramany <almumadra@hotmail.com>
2025-02-04 09:48:50 +0100

    header css ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 
 import "../styles/header.css";
+import logoTR from "../images/logo_ok.png";
+import logoMenu from "../images/menu-icon.png";
 
 function header() {
   return (
     <>
+    <header className="header">
     <div className='header_contact'>
         <p>+34 623 91 26 06 | contacto@trpsicologia.com</p>
         <p>Avenida de América / Diego de León</p>
@@ -13,6 +16,27 @@ function header() {
         <button className='header_btn'>QUIERO UNA CITA</button>
         </a>
     </div>
+
+    <div className="header_menu">
+
+        <div >
+            <img src={logoTR} alt="logo TR psicologia" className="header_logo"/>
+        </div>
+
+        <nav className="header_menu">
+            <img src={logoMenu} alt="logo-menu" className="menu_logo"/>
+      
+            <div className="main_menu">
+                <ul className="menu">
+                        <li>INICIO</li>
+                        <li>CONÓCENOS</li>
+                        <li>TERAPIAS</li>
+                        <li>TARIFAS</li> 
+                    </ul>
+                </div>
+        </nav>
+    </div>
+    </header>
 
     </>
   )
